7f35aab93d63927f6142564e79b3fcf36a1d7981
Mohamed Albgal <malbgal@gmail.com>
2021-12-25 13:16:28 -0800

    fixed only save on change

--- src/components/TextBoxBody.js
+++ src/components/TextBoxBody.js
@@ -7,15 +7,20 @@ import { StoreContext } from '../store/store'
 const TextAreaContainer = tw.div`block mx-auto h-screen`
 const TextArea = tw.textarea` block mx-auto p-10 w-11/12 h-96 max-h-screen font-mono overflow-scroll whitespace-pre-wrap`
 const CommitButton = tw(Button)`h-10 w-32 font-thin mt-6`
-const SavingIndicator = tw(CommitButton)`from-yellow-50 to-yellow-500 `
 
 const TextBoxBody = ({link, saveAction}) => {
     const [ body, setBody ] = useState(link.linkNotes)
     const { state } = useContext(StoreContext);
+    
+    const saveChanges = () => {
+        // only save if changes made
+        if (link.linkNotes !== body)
+            saveAction({...link, linkNotes: body})
+    }
     return (
         <TextAreaContainer>            
-            <TextArea value={body} onChange={e => setBody(e.target.value)}     />
-            { state.loading ? <SavingIndicator /> : <CommitButton onClick={() => saveAction({...link, linkNotes: body})}>Save</CommitButton>}
+            <TextArea value={body} onChange={e => setBody(e.target.value)} />
+            {state.loading ? null: <CommitButton onClick={saveChanges}>Save</CommitButton>}
         </TextAreaContainer>
     )
 }
--- src/components/containers/RightSide.js
+++ src/components/containers/RightSide.js
@@ -1,13 +1,10 @@
 import React, { useContext } from 'react';
-import { LinkContext, SelectedLinkContext } from '../../Contexts';
 import tw from 'twin.macro';
 import TextBoxBody from '../TextBoxBody';
 import { StoreContext } from '../../store/store';
 
 const Container = tw.div`w-2/3 bg-gray-100 relative flex-1 p-10`;
 const NoteTitle= tw.div`text-xl font-semibold text-center`
-const SpinnerContainer = tw.div` bg-transparent  opacity-70  p-2 h-20 w-20 top-1/4 right-1/2 rounded-full absolute `
-const Spinner = tw.div`animate-spin rounded-full absolute  w-full h-full  p-2  border-t-4 border-r-4 border-purple-700 `
 
 const RightSide =  () => {
     const { state,actions } = useContext(StoreContext)
